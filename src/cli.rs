190d0d3eaa4d9c150a50cadb038b2317aa3f9d73
Vitor Lobo Ramos <lobocode@gmail.com>
2024-10-16 13:33:25 -0300

    clap parser parameters

--- src/cli.rs
+++ src/cli.rs
@@ -1,40 +1,28 @@
-use structopt::StructOpt;
-use std::path::PathBuf;
+use clap::Parser;
 
-#[derive(StructOpt, Debug)]
-#[structopt(name = "Marmite Site Generator", about = "Generates static websites from markdown content")]
+/// Marmite Site Generator
+/// Generates static websites from markdown content
+#[derive(Parser, Debug)]
+#[command(name = "Marmite Site Generator", about = "Generates static websites from markdown content")]
 pub struct Cli {
-    /// The root folder of the site
-    pub folder: Option<String>,
+    /// The root folder of the site (input folder)
+    pub input_folder: Option<String>,
+
+    /// Build the website (render markdown to HTML)
+    #[arg(long)]
+    pub build: bool,
 
     /// Serve the website in editing mode
-    #[structopt(long)]
+    #[arg(long)]
     pub serve: bool,
 
-    /// Initialize the folder structure
-    #[structopt(long)]
-    pub init: Option<String>,
-
     /// Custom configuration file path
-    #[structopt(long, value_name = "FILE")]
+    #[arg(long, value_name = "FILE")]
     pub config: Option<String>,
 }
 
 impl Cli {
-    pub fn get_folder(&self) -> PathBuf {
-        if let Some(folder_name) = &self.init {
-            PathBuf::from(folder_name)
-        } else if self.serve {
-            PathBuf::from(self.folder.as_ref().map(|s| s.as_str()).unwrap_or("."))
-        } else {
-            PathBuf::from(self.folder.as_ref().expect("Folder argument is required unless initializing a project or serving."))
-        }
-    }
-
-    pub fn get_config_path(&self) -> PathBuf {
-        self.config
-            .as_ref()
-            .map(PathBuf::from)
-            .unwrap_or_else(|| PathBuf::from("marmite.yaml"))
+    pub fn parse() -> Self {
+        <Self as Parser>::parse()
     }
 }
