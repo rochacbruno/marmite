a829678707589ef0d58d0eb1454d15703ef392dc
Akash <akashsingh2210670@gmail.com>
2024-10-27 22:56:30 +0800

    resolved comments and refactored code in templates.rs

--- src/templates.rs
+++ src/templates.rs
@@ -1,37 +1,42 @@
 use log::error;
 use std::fs;
 use std::path::PathBuf;
+use crate::embedded::{Templates, generate_static};
 
-/// Creates the `templates/` folder and base template file.
-pub fn initialize_templates(output_folder: &PathBuf) {
-    let templates_path = output_folder.join("templates");
+/// Creates the `templates/` folder and writes embedded templates to it.
+pub fn initialize_templates(input_folder: &PathBuf) {
+    let templates_path = input_folder.join("templates");
     if let Err(e) = fs::create_dir_all(&templates_path) {
         error!("Failed to create templates directory: {}", e);
         return;
     }
 
-    let base_template_path = templates_path.join("base.html");
-    if let Err(e) = fs::write(base_template_path, "<!-- Base HTML template -->") {
-        error!("Failed to create base template: {}", e);
+    for name in Templates::iter() {
+        if let Some(template) = Templates::get(name.as_ref()) {
+            if let Ok(template_str) = std::str::from_utf8(template.data.as_ref()) {
+                let template_path = templates_path.join(name.as_ref());
+                if let Err(e) = fs::write(&template_path, template_str) {
+                    error!("Failed to write template '{}': {}", name.as_ref(), e);
+                }
+            } else {
+                error!("Failed to parse template '{}' as UTF-8", name.as_ref());
+            }
+        } else {
+            error!("Template '{}' not found in embedded resources", name.as_ref());
+        }
     }
 }
 
-/// Creates the `static/` folder with basic placeholder files.
-pub fn initialize_theme_assets(output_folder: &PathBuf) {
-    let static_path = output_folder.join("static");
-    if let Err(e) = fs::create_dir_all(&static_path) {
-        error!("Failed to create static assets directory: {}", e);
-        return;
+/// Uses `generate_static` to populate the `static/` folder with embedded content.
+pub fn initialize_theme_assets(input_folder: &PathBuf) {
+    let static_path = input_folder.join("static");
+
+    if !static_path.exists() {
+        if let Err(e) = fs::create_dir_all(&static_path) {
+            error!("Failed to create static assets directory: {}", e);
+            return;
+        }
     }
 
-    let css_path = static_path.join("style.css");
-    let js_path = static_path.join("script.js");
-
-    if let Err(e) = fs::write(css_path, "/* Add your styles here */") {
-        error!("Failed to create CSS file: {}", e);
-    }
-
-    if let Err(e) = fs::write(js_path, "// Add your scripts here") {
-        error!("Failed to create JavaScript file: {}", e);
-    }
+    generate_static(&static_path);
 }
